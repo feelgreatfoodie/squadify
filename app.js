73958bb419da5c906301f86e65f7c520d35146de
Eric Budd <ericbudd@gmail.com>
2018-04-17 11:47:26 -0600

    update login routing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventsRouter = require('./routes/events');
-var registerRouter = require('./routes/register')
+var loginRouter = require('./routes/login')
 
 var app = express();
 
@@ -17,14 +17,16 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter)
-app.use('/register', registerRouter)
+app.use('/login', loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'Register' });
+  res.render('login', {
+    title: 'Login'
+  });
 });
 
 module.exports = router;
