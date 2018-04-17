2deb4f3d885dab59ad9559d3360f8b365247e746
Eric Budd <ericbudd@gmail.com>
2018-04-17 16:51:23 -0600

    remove logging

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("node env=", process.env.NODE_ENV);
-  console.log("JWT_KEY=", process.env.JWT_KEY);
   res.render('login', {
     title: 'Login'
   });
