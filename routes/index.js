5c26cd9f95d77e5509317f4d5a1cae432217d666
Eric Budd <ericbudd@gmail.com>
2018-04-19 08:51:49 -0600

    first name used when login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WarDogs' });
+
+  let userFirstName = 'adventurer'
+
+  if (req.cookies.token !== undefined) {
+    console.log("req.cookies.token", req.cookies.token);
+    console.log("process.env.JWT_KEY).id", process.env.JWT_KEY)
+    userFirstName = jwt.verify(req.cookies.token, process.env.JWT_KEY).first_name
+  }
+
+
+  res.render('index', {
+    title: 'WarDogs',
+    userFirstName: userFirstName
+  });
 });
 
 module.exports = router;
