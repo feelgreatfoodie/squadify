64464ee637eaada771700a093b727013e53958d7
Christian Bourlier <chrisbourlier@hotmail.com>
2018-04-19 11:19:32 -0600

    assets added and route modified

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,16 +7,17 @@ router.get('/', function(req, res, next) {
   let userFullName = 'User name'
 
   if (req.cookies.token !== undefined) {
-    console.log("req.cookies.token", req.cookies.token);
-    console.log("process.env.JWT_KEY).id", process.env.JWT_KEY)
+    // console.log("req.cookies.token", req.cookies.token);
+    // console.log("process.env.JWT_KEY).id", process.env.JWT_KEY)
     tokenObject = jwt.verify(req.cookies.token, process.env.JWT_KEY)
-    console.log(tokenObject);
+    console.log('hola, me llamo: ', tokenObject);
     userFullName = `${tokenObject.first_name} ${tokenObject.last_name}`
   }
 
   res.render('dash', {
     title: 'Dashboard',
-    userFullName: userFullName
+    userFullName: userFullName,
+    userImg: `${tokenObject.image_url}`
   });
 });
 
