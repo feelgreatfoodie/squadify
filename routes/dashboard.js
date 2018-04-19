a9bb44db168e5599040a4113d1a19a0fd40bd7f4
Eric Budd <ericbudd@gmail.com>
2018-04-19 09:24:23 -0600

    dashboard user names

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dash', { title: 'Dashboard' });
+  let userFullName = 'User name'
+
+  if (req.cookies.token !== undefined) {
+    console.log("req.cookies.token", req.cookies.token);
+    console.log("process.env.JWT_KEY).id", process.env.JWT_KEY)
+    tokenObject = jwt.verify(req.cookies.token, process.env.JWT_KEY)
+    console.log(tokenObject);
+    userFullName = `${tokenObject.first_name} ${tokenObject.last_name}`
+  }
+
+  res.render('dash', {
+    title: 'Dashboard',
+    userFullName: userFullName
+  });
 });
 
 module.exports = router;
