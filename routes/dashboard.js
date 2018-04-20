47329f4ea989d1b912d2afd2265abd896cd9823d
Eric Budd <ericbudd@gmail.com>
2018-04-20 09:11:46 -0600

    get badges working for both host and join

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,21 +7,42 @@ const knex = require('../knex')
 
 const getHostingCount = (req, res, next) => {
   res.locals.hostingCount = ''
-  const users_id = jwt.verify(req.cookies.token, process.env.JWT_KEY).id
-  knex('users')
+  const owner_id = jwt.verify(req.cookies.token, process.env.JWT_KEY).id
+  knex('events')
     .where({
-      id: users_id,
+      owner_id: owner_id,
     })
     .count('id')
+    .first()
     .then(count => {
-      res.locals.hostingCount = count
+      console.log("count", count.count)
+      if (count.count > 0) {
+        res.locals.hostingCount = count.count
+      } else {
+        res.locals.hostingCount = ""
+      }
+      next()
     })
-  next()
 }
 
 const getParticipatingCount = (req, res, next) => {
   res.locals.participatingCount = ''
-  next()
+  const user_id = jwt.verify(req.cookies.token, process.env.JWT_KEY).id
+  knex('events_users')
+    .where({
+      users_id: user_id,
+    })
+    .count('users_id')
+    .first()
+    .then(count => {
+      console.log("count", count.count)
+      if (count.count > 0) {
+        res.locals.participatingCount = count.count
+      } else {
+        res.locals.participatingCount = ""
+      }
+      next()
+    })
 }
 
 
@@ -30,10 +51,15 @@ const renderDash = (req, res, next) => {
   // let tokenObject = ''
   // let about_user = ''
 
+  console.log("second res.locals.hostingCount");
+
   const tokenObject = jwt.verify(req.cookies.token, process.env.JWT_KEY)
   const userFullName = `${tokenObject.first_name} ${tokenObject.last_name}`
   const about_user = tokenObject.about_user
   const userImg = tokenObject.image_url
+
+
+
 
   res.render('dash', {
     title: 'Dashboard',
