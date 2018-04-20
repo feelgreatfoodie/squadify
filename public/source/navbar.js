be1c08df14c6b016b06ba607fa5a159fbd4e1b68
Zach Stevens <zachstevens39@gmail.com>
2018-04-20 10:32:24 -0600

    Added mountain logo

--- public/source/navbar.js
+++ public/source/navbar.js
@@ -14,8 +14,7 @@ $(document).ready(function () {
       if (inOrOut === 'login') {
         loginOrSignoutText = 'Log in / Register'
         loginLink = '/login'
-      }
-      else if (inOrOut === 'signout') {
+      } else if (inOrOut === 'signout') {
         loginOrSignoutText = 'Sign out'
         loginLink = '/signout'
       }
@@ -30,7 +29,10 @@ $(document).ready(function () {
       }
       $('#pagenavbar').append(`
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Squadify</a>
+      <a class="navbar-brand" href="/">
+        <img src="http://g81-wardogs.herokuapp.com/assets/mountain.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Squadify
+      </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
