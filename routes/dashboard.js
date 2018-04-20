c5ac589b482c389397ee93547e59792d068e755f
Eric Budd <ericbudd@gmail.com>
2018-04-20 08:38:20 -0600

    dashboard counts setup

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
+const knex = require('../knex')
 
 /* GET home page. */
+
+const getHostingCount = (req, res, next) => {
+  res.locals.hostingCount = ''
+  const users_id = jwt.verify(req.cookies.token, process.env.JWT_KEY).id
+  knex('users')
+    .where({
+      id: users_id,
+    })
+    .count('id')
+    .then(count => {
+      res.locals.hostingCount = count
+    })
+  next()
+}
+
+const getParticipatingCount = (req, res, next) => {
+  res.locals.participatingCount = ''
+  next()
+}
+
 
 const renderDash = (req, res, next) => {
   let userFullName = 'User name'
   let tokenObject = ''
   let about_user = ''
-  let hostingCount = ""
-  let participatingCount = ""
 
   if (req.cookies.token !== undefined) {
     tokenObject = jwt.verify(req.cookies.token, process.env.JWT_KEY)
@@ -22,17 +41,13 @@ const renderDash = (req, res, next) => {
     userFullName: userFullName,
     userImg: `${tokenObject.image_url}`,
     about_user: about_user,
-    hostingCount: hostingCount,
-    participatingCount: participatingCount
+    hostingCount: res.locals.hostingCount,
+    participatingCount: res.locals.participatingCount
   })
 }
 
 
 
-
-
-
-
-router.get('/', renderDash)
+router.get('/', getHostingCount, getParticipatingCount, renderDash)
 
 module.exports = router;
