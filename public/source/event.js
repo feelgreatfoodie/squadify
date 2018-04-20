dccd50abc3fc2e185b0cae21809b0a44f77221a2
Eric Budd <ericbudd@gmail.com>
2018-04-20 09:59:22 -0600

    snackbar setup for event

--- public/source/event.js
+++ public/source/event.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
   'use strict';
 
+  function myFunction() {
+    // Get the snackbar DIV
+    var x = document.getElementById("joinButton");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function() {
+      x.className = x.className.replace("show", "");
+    }, 3000);
+  }
+
+
   // eslint-disable-next-line max-statements
   $('#joinButton').click((event) => {
     event.preventDefault();
