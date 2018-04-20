a343f1c9eec4ee2c1b7dd610c95e9e3910ee998d
Eric Budd <ericbudd@gmail.com>
2018-04-20 08:41:09 -0600

    fix dash conflicts

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -26,32 +26,25 @@ const getParticipatingCount = (req, res, next) => {
 
 
 const renderDash = (req, res, next) => {
-    let userFullName = 'User name'
-    let tokenObject = ''
-    let about_user = ''
+  // let userFullName = 'User name'
+  // let tokenObject = ''
+  // let about_user = ''
 
-    const tokenObject = jwt.verify(req.cookies.token, process.env.JWT_KEY)
-    const userFullName = `${tokenObject.first_name} ${tokenObject.last_name}`
-    const about_user = tokenObject.about_user
-    const userImg = tokenObject.image_url
+  const tokenObject = jwt.verify(req.cookies.token, process.env.JWT_KEY)
+  const userFullName = `${tokenObject.first_name} ${tokenObject.last_name}`
+  const about_user = tokenObject.about_user
+  const userImg = tokenObject.image_url
 
-    res.render('dash', {
-      title: 'Dashboard',
-      userFullName: userFullName,
-      userImg: `${userImg}`,
-      about_user: about_user,
-      hostingCount: res.locals.hostingCount,
-      participatingCount: res.locals.participatingCount
-    })
-  }
+  res.render('dash', {
+    title: 'Dashboard',
+    userFullName: userFullName,
+    userImg: `${userImg}`,
+    about_user: about_user,
+    hostingCount: res.locals.hostingCount,
+    participatingCount: res.locals.participatingCount
+  })
+}
 
-  <<
-  << << < HEAD
-
-
-router.get('/', getHostingCount, getParticipatingCount, renderDash) ===
-  === =
-  router.get('/', renderDash) >>>
-  >>> > 22 f6ab9d521417b2fa6150c09b73d64f37e44138
+router.get('/', getHostingCount, getParticipatingCount, renderDash)
 
 module.exports = router;
