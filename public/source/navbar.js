5e34c0bd6f9c0a730bab6f67178242efbc89deae
Christian Bourlier <chrisbourlier@hotmail.com>
2018-04-20 09:43:21 -0600

    github link/icon added to all footers

--- public/source/navbar.js
+++ public/source/navbar.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
       }
       $('#pagenavbar').append(`
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">WarDogs</a>
+        <a class="navbar-brand" href="/">Squadify</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
